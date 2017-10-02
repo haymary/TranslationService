6d3fc4585d319df84370c2e3bc902e4059a203c1
Maria <marikoreneva@gmail.com>
2017-10-02 14:37:41 +0300

    Fixed some bugs in test. Made text preprocessing for yandex

--- src/translators/YandexTranslator.py
+++ src/translators/YandexTranslator.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 
 from src.translators.ATranslator import ATranslator
@@ -7,6 +9,7 @@ class YandexTranslator(ATranslator):
 	api_key = 'trnsl.1.1.20170926T124116Z.2bd1508a7ffae706.5cbed5dc27c8ea5b76a36ad193fcd826fbe1a366'
 
 	def translate(self, source_lang, source_text, target_lang):
+		source_text = self.escape_special_chars(source_text)
 		request = 'https://translate.yandex.net/api/v1.5/tr.json/translate?' \
 		          'key=' + self.api_key +\
 		          '&text=' + source_text +\
@@ -22,5 +25,14 @@ class YandexTranslator(ATranslator):
 			except Exception as e:
 				print(e)
 				return None
-			print(translation.json())
 		return translation.json()['text'][0]
+
+	def escape_special_chars(self, source_text):
+		return source_text.translate(str.maketrans({"-":  r"\-",
+                                          "]":  r"\]",
+                                          "\\": r"\\",
+                                          "^":  r"\^",
+                                          "$":  r"\$",
+                                          "*":  r"\*"}))
+
+
--- test/translators/TestYandexTranslator.py
+++ test/translators/TestYandexTranslator.py
@@ -13,10 +13,19 @@ class TestYandexTranslator(unittest.TestCase):
 		source = 'ru'
 		target = 'en'
 
-		translation = self.yt.translate(source, target, text)
+		translation = self.yt.translate(source, text, target)
 
 		if translation is not None:
 			print(translation)
 		else:
 			print("Error happened")
 		self.assertEqual('Note. All special characters must be escaped.', translation)
+
+	def test_with_special_characters(self):
+		text = 'Примечание. " : % ^ & Все специальные символы должны быть экранированы.'
+		source = 'ru'
+		target = 'en'
+
+		translation = self.yt.translate(source, text, target)
+		print(translation)
+		self.assertNotEqual(translation, None)
