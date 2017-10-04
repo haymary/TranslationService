0baaff708c85514612009366c3d0edd0324133c2
Maria <marikoreneva@gmail.com>
2017-10-04 19:12:11 +0300

    Added identity check

--- src/translators/YandexTranslator.py
+++ src/translators/YandexTranslator.py
@@ -18,25 +18,34 @@ class YandexTranslator(ATranslator):
 		"""
 		source_text = self._escape_special_chars(source_text)
 		request = 'https://translate.yandex.net/api/v1.5/tr.json/translate?' \
-		          'key=' + self._api_key +\
-		          '&text=' + source_text +\
+		          'key=' + self._api_key + \
+		          '&text=' + source_text + \
 		          '&lang=' + source_lang + '-' + target_lang
 
 		no_res = True
-		while no_res is True:
-			try:
+		counter = 0
+		try:
+			while no_res is True:
 				translation = requests.post(request)
-				no_res = False
-			except requests.exceptions.ConnectionError as e:
+				if 'code' not in translation.json():
+					no_res = False
+				counter += 1
+				if counter > 10:
+					break
+			js = translation.json()
+			print(js)
+			if 'code' in js:
 				return None
-			except Exception as e:
-				print(e)
-				return None
-		print(translation.json())
-		return translation.json()['text'][0]
+			return translation.json()['text'][0]
+		except requests.exceptions.ConnectionError as e:
+			print(e)
+			return None
+		except Exception as e:
+			print(e)
+			return None
 
 	def _escape_special_chars(self, source_text):
-		return source_text._translate(str.maketrans({
+		return source_text.translate(str.maketrans({
 			"-": r"\-",
 			"]": r"\]",
 			"\\": r"\\",
--- test/TestTranslationSevice.py
+++ test/TestTranslationSevice.py
@@ -27,7 +27,7 @@ class TestTranslatorService(unittest.TestCase):
 
 		translation = self.ts._translate(source_lang, source_text, target_lang)
 		translation_db = self.ts._db.get_translation(source_lang, source_text, target_lang)
-		self.assertEqual(translation, translation_db)
+		self.assertEqual(translation[0], translation_db)
 
 	def test_data_saved_to_cache(self):
 		source_lang = 'en'
@@ -36,13 +36,13 @@ class TestTranslatorService(unittest.TestCase):
 
 		translation = self.ts._translate(source_lang, source_text, target_lang)
 		translation_c = self.ts._cache.get_translation(source_lang, source_text, target_lang)
-		self.assertEqual(translation, translation_c)
+		self.assertEqual(translation[0], translation_c)
 
 	def test_translate_many(self):
 		source_lang = 'en'
 		source_text = 'Hello, My name is Maria'
 		target_langs = ['ru', 'it', 'ja']
-		tr_json = {'source_lang':source_lang, 'source_text':source_text, 'target_langs':target_langs}
+		tr_json = {"user_info": 0, 'source_lang':source_lang, 'source_text':source_text, 'target_langs':target_langs}
 		translations = json.loads(self.ts.translate_json(tr_json))
 		for tr in translations:
 			self.assertNotEqual(tr, None)
@@ -54,12 +54,13 @@ class TestTranslatorService(unittest.TestCase):
 		source_text = 'Примечание. Все специальные символы должны быть экранированы.'
 		target_langs = ['en']
 		target_text = 'Note. All special characters must be escaped.'
-		tr_json = {'source_lang': source_lang, 'source_text': source_text, 'target_langs': target_langs}
+		tr_json = {"user_info": 0, 'source_lang': source_lang, 'source_text': source_text, 'target_langs': target_langs}
 		translations = json.loads(self.ts.translate_json(tr_json))
 		self.assertEqual(translations['translation'][0], target_text)
 
 	def test_translate_many_not_none(self):
 		req = {
+			"user_info": 0,
 			"source_lang": "en",
 			"source_text": "Hello, My name is Maria",
 			"target_langs": [
